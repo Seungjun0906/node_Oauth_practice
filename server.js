1e866405a69f74a767e5e74aec7b7885c80022da
Seungjun <seungjun.kim0906@gmail.com>
2021-12-16 18:55:33 -0500

    Learning Oauth

---
+++ server.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret value is 42");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+
+// openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
